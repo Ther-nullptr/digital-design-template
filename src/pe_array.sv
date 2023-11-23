ae5f37a17c9d1a91f1aaa918937b217a4f37f4ec
Ther <1329438302@qq.com>
2023-11-23 15:14:08 +0800

    chore: format

--- src/pe_array.sv
+++ src/pe_array.sv
@@ -1,33 +1,34 @@
+`timescale 1ns/1ps
 module pe_array #(
-	parameter MAC_NUM    =   10,  // number of multiply-accumulation units
-	parameter BW_ACT     =   8,  // bit length of activation
-    parameter BW_WET     =   8,  // bit length of weight
-    parameter BW_ACCU    =   32    // bit length of accu result    
-)(
-    input        clk,
-    input        reset_n,
-    // // control signal
-    input        PE_mac_enable,      // high active
-    input        PE_clear_acc,
-    // data signal 
-    input   signed     [BW_ACT-1:0]    PE_act_in [MAC_NUM-1:0] ,         // input activation
-    input   signed     [BW_WET-1:0]    PE_wet_in,         // input weight
-    input              [7:0]           PE_res_shift_num,
-    output  reg signed [BW_ACT-1:0]    PE_result_out [MAC_NUM-1:0]     // output result   
-);
+        parameter MAC_NUM    =   10,  // number of multiply-accumulation units
+        parameter BW_ACT     =   8,  // bit length of activation
+        parameter BW_WET     =   8,  // bit length of weight
+        parameter BW_ACCU    =   32    // bit length of accu result
+    )(
+        input        clk,
+        input        reset_n,
+        // // control signal
+        input        PE_mac_enable,      // high active
+        input        PE_clear_acc,
+        // data signal
+        input   signed     [BW_ACT-1:0]    PE_act_in [MAC_NUM-1:0] ,         // input activation
+        input   signed     [BW_WET-1:0]    PE_wet_in,         // input weight
+        input              [7:0]           PE_res_shift_num,
+        output  reg signed [BW_ACT-1:0]    PE_result_out [MAC_NUM-1:0]     // output result
+    );
 
     reg signed [BW_ACT-1:0]    PE_act_in_reg [MAC_NUM-1:0] ;      // input buffer
     reg signed [BW_WET-1:0]    PE_wet_in_reg;      // weight buffer
-    reg signed [BW_ACCU-1:0]   PE_result_out_reg [MAC_NUM-1:0];  
+    reg signed [BW_ACCU-1:0]   PE_result_out_reg [MAC_NUM-1:0];
     reg PE_clear_acc_reg; //数据信号因需要存入buffer而延后了1 cycle，该控制信号也延后1 cycle
 
     always @(posedge clk or negedge reset_n) begin
-        if(~reset_n) begin 
+        if(~reset_n) begin
             PE_clear_acc_reg <= '0;
         end
         else begin
             PE_clear_acc_reg <= PE_clear_acc;
-        end        
+        end
     end
 
     always @(posedge clk or negedge reset_n) begin
