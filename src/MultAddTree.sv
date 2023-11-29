6f0381487b14971a65c2678d403d36edec66feda
Ther <1329438302@qq.com>
2023-11-29 14:05:27 +0800

    chore: finish multaddtree

--- src/MultAddTree.sv
+++ src/MultAddTree.sv
@@ -4,33 +4,72 @@
 // Date: 2023.11.23
 // Note: mult-add tree, the interface is same as pe_array.sv
 //////////////////////////////////////////////////////////////////////////
+`timescale 1ns/1ps
 
 module MultAddTree # (
-  parameter MAC_NUM_LOG2 =   4,  // log2 number of multiply-accumulation units. e.g. if the MAC number is 16, then MAC_NUM_LOG2 = 4
-  parameter BW_ACT       =   8,  // bit length of activation
-  parameter BW_WET       =   8,  // bit length of weight
-  parameter BW_ACCU      =   32  // bit length of accu result
+  parameter MAC_NUM             =   10, // number of multiply-accumulation units
+  parameter ACCU_NUM_LOG2       =   4,  // log2 number of multiply-accumulation units. e.g. if the MAC number is 16, then ACCU_NUM_LOG2 = 4
+  parameter BW_ACT              =   8,  // bit length of activation
+  parameter BW_WET              =   8,  // bit length of weight
+  parameter BW_ACCU             =   32, // bit length of accu result
+  localparam ACCU_NUM           =   (1 << ACCU_NUM_LOG2)
 ) (
   input  clk,
   input  reset_n,
   input  PE_mac_enable,
   input  PE_clear_acc,
-  input  signed     [BW_ACT-1:0]    PE_act_in [(1 << MAC_NUM_LOG2)-1:0],
-  input  signed     [BW_WET-1:0]    PE_wet_in [(1 << MAC_NUM_LOG2)-1:0],
+  input  signed     [BW_ACT-1:0]    PE_act_in [MAC_NUM-1:0][ACCU_NUM-1:0],
+  input  signed     [BW_WET-1:0]    PE_wet_in [ACCU_NUM-1:0],
   input             [7:0]           PE_res_shift_num,
-  output reg signed [BW_ACT-1:0]    PE_result_out
+  output reg signed [BW_ACT-1:0]    PE_result_out [MAC_NUM-1:0]
 );
-  localparam MAC_NUM = 1 << MAC_NUM_LOG2;
-  localparam PE_MEDIUM_RESULT_REG = 2 * MAC_NUM - 1;
+  // localparam ACCU_NUM = 1 << ACCU_NUM_LOG2;
+  localparam PE_MEDIUM_RESULT_REG = 2 * ACCU_NUM - 1;
 
-  reg signed [BW_ACT-1:0]    PE_act_in_reg [MAC_NUM-1:0];      // input buffer
-  reg signed [BW_WET-1:0]    PE_wet_in_reg [MAC_NUM-1:0];      // weight buffer
-  reg signed [BW_ACCU-1:0]   PE_result_medium_result_reg [PE_MEDIUM_RESULT_REG-1:0];
+  reg signed [BW_ACT-1:0]    PE_act_in_reg [MAC_NUM-1:0][ACCU_NUM-1:0];      // input buffer
+  reg signed [BW_WET-1:0]    PE_wet_in_reg [ACCU_NUM-1:0];      // weight buffer
+  reg signed [BW_ACCU-1:0]   PE_result_medium_result_reg [MAC_NUM-1:0][PE_MEDIUM_RESULT_REG-1:0];
+  reg signed [BW_ACCU-1:0]   PE_result_out_reg [MAC_NUM-1:0];
   reg PE_clear_acc_reg; // delay 1 cycle for control signal
 
-  wire signed [BW_ACCU-1:0] PE_result_shift_temp;
+  // only for waveform
+  genvar i,j;
+  generate
+    for (i = 0; i < MAC_NUM; i = i + 1) begin : medium_result
+      for (j = 0; j < PE_MEDIUM_RESULT_REG; j = j + 1) begin : medium_result_inside
+        wire signed [BW_ACCU-1:0] PE_result_medium_result_reg_tmp;
+        assign PE_result_medium_result_reg_tmp = PE_result_medium_result_reg[i][j];
+      end
+    end
+  endgenerate
 
-  // control signal for clear accu
+  genvar gv_weight;
+  generate
+    for (gv_weight = 0; gv_weight < ACCU_NUM; gv_weight = gv_weight + 1) begin : wet_in
+        wire signed [BW_WET-1:0] PE_wet_in_tmp;
+        assign PE_wet_in_tmp = PE_wet_in_reg[gv_weight];
+    end
+  endgenerate
+
+  genvar gv_input, accu_i;
+  generate
+    for (gv_input = 0; gv_input < MAC_NUM; gv_input = gv_input + 1) begin : act_in
+        for (accu_i = 0; accu_i < ACCU_NUM; accu_i = accu_i + 1) begin : accu
+            wire signed [BW_ACT-1:0] PE_act_in_tmp;
+            assign PE_act_in_tmp = PE_act_in_reg[gv_input][accu_i];
+        end
+    end
+  endgenerate
+
+  genvar gv_output;
+  generate
+    for (gv_output = 0; gv_output < MAC_NUM; gv_output = gv_output + 1) begin : act_output
+        wire signed [BW_ACT-1:0] PE_result_out_tmp;
+        assign PE_result_out_tmp = PE_result_out[gv_output];
+    end
+  endgenerate
+
+  // control signal for clear accu, delay for 2log2(ACCU_NUM) + 1 cycles
   always @(posedge clk or negedge reset_n) begin
     if(~reset_n) begin
       PE_clear_acc_reg <= 'b0;
@@ -40,99 +79,113 @@ module MultAddTree # (
     end
   end
 
-  // buffer the activation
-  always @(posedge clk or negedge reset_n) begin
-    if(~reset_n) begin // reset to zero
-      for (integer i = 0; i < MAC_NUM; i = i + 1) begin
-        PE_act_in_reg[i] <= 'b0;
-      end
-    end
-    else begin
-      for (integer i = 0; i < MAC_NUM; i = i + 1) begin
-        PE_act_in_reg[i] <= PE_act_in[i];
-      end
-    end
-  end
-
   // buffer the weight
   always @(posedge clk or negedge reset_n) begin
     if(~reset_n) begin // reset to zero
-      for (integer i = 0; i < MAC_NUM; i = i + 1) begin
+      for (integer i = 0; i < ACCU_NUM; i = i + 1) begin
         PE_wet_in_reg[i] <= 'b0;
       end
     end
     else begin
-      for (integer i = 0; i < MAC_NUM; i = i + 1) begin
+      for (integer i = 0; i < ACCU_NUM; i = i + 1) begin
         PE_wet_in_reg[i] <= PE_wet_in[i];
       end
     end
   end
 
-  // MAC tree logic -- multiply
-  genvar gv_i;
-  generate 
-    for (gv_i = 0; gv_i < MAC_NUM; gv_i = gv_i + 1) begin : multiply
-      always @(posedge clk or negedge reset_n) begin
-        if(~reset_n) begin // reset to zero
-          PE_result_medium_result_reg[gv_i] <= 'b0;
-        end
-        else if(~PE_mac_enable) begin // non-calculation time, keep the same
-          PE_result_medium_result_reg[gv_i] <= PE_result_medium_result_reg[gv_i];
-        end
-        else if(PE_clear_acc_reg) begin
-          PE_result_medium_result_reg[gv_i] <= 'b0;
-        end
-        else begin // multiply and accumulate
-          PE_result_medium_result_reg[gv_i] <= PE_wet_in_reg[gv_i] * PE_act_in_reg[gv_i];
-        end
-      end 
-    end
-  endgenerate
+  // # MAC_NUM trees
+  genvar gv_i, buffer_i, mult_i, stage, gv_j;
+  generate
+    for (gv_i = 0; gv_i < MAC_NUM; gv_i = gv_i + 1) begin : line
+      wire signed [BW_ACCU-1:0] PE_result_shift_temp;
 
-  // MAC tree logic -- pipelined accumulate
-  genvar stage;
-  generate 
-    for (stage = 0; stage <= MAC_NUM_LOG2; stage = stage + 1) begin : accumulate // note the last stage is accumulate the result into the output register
-      localparam STAGE_INPUT_BEGIN_INDEX = (1 << MAC_NUM_LOG2) - (1 << (MAC_NUM_LOG2 - stage));
-      localparam STAGE_OUTPUT_BEGIN_INDEX = (1 << MAC_NUM_LOG2) - (1 << (MAC_NUM_LOG2 - stage + 1));
-      genvar gv_j;
-      for (gv_j = 0; gv_j < STAGE_INPUT_NUM; gv_j = gv_j + 2) begin : line
+      // buffer the activation
+      for (buffer_i = 0; buffer_i < ACCU_NUM; buffer_i = buffer_i + 1) begin : buffer
         always @(posedge clk or negedge reset_n) begin
           if(~reset_n) begin // reset to zero
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j] <= 'b0;
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX+ gv_j + 1] <= 'b0;
+            PE_act_in_reg[gv_i][buffer_i] <= 'b0;
+          end
+          else begin
+            PE_act_in_reg[gv_i][buffer_i] <= PE_act_in[gv_i][buffer_i];
+          end
+        end
+      end
+
+      // MAC tree logic -- multiply
+      for (mult_i = 0; mult_i < ACCU_NUM; mult_i = mult_i + 1) begin : multiply
+        always @(posedge clk or negedge reset_n) begin
+          if(~reset_n) begin // reset to zero
+            PE_result_medium_result_reg[gv_i][mult_i] <= 'b0;
           end
           else if(~PE_mac_enable) begin // non-calculation time, keep the same
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j] <= PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j];
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j + 1] <= PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j + 1];
+            PE_result_medium_result_reg[gv_i][mult_i] <= PE_result_medium_result_reg[gv_i][mult_i];
           end
-          else if(PE_clear_acc_reg) begin
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j] <= 'b0;
-            PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j + 1] <= 'b0;
+          else if(PE_clear_acc_reg) begin // clear the result
+            PE_result_medium_result_reg[gv_i][mult_i] <= 'b0;
           end
-          else begin // accumulate
-            PE_result_medium_result_reg[STAGE_OUTPUT_BEGIN_INDEX + (gv_j >> 1)] <= PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j] + PE_result_medium_result_reg[STAGE_INPUT_BEGIN_INDEX + gv_j + 1];
+          else begin // multiply and accumulate
+            PE_result_medium_result_reg[gv_i][mult_i] <= PE_wet_in_reg[mult_i] * PE_act_in_reg[gv_i][mult_i];
           end
+        end 
+      end
+
+      // MAC tree logic -- pipelined accumulate
+      for (stage = 0; stage < ACCU_NUM_LOG2; stage = stage + 1) begin : accumulate // note the last stage is accumulate the result into the output register
+        localparam STAGE_INPUT_BEGIN_INDEX = (1 << (ACCU_NUM_LOG2 + 1)) - (1 << ((ACCU_NUM_LOG2 + 1) - stage));
+        localparam STAGE_OUTPUT_BEGIN_INDEX = (1 << (ACCU_NUM_LOG2 + 1)) - (1 << ((ACCU_NUM_LOG2 + 1) - (stage + 1)));
+        localparam STAGE_INPUT_NUM = (1 << (ACCU_NUM_LOG2 - stage));
+          for (gv_j = 0; gv_j < STAGE_INPUT_NUM; gv_j = gv_j + 2) begin : stage_inside
+            always @(posedge clk or negedge reset_n) begin
+            if(~reset_n) begin // reset to zero
+              // clear all the intermediate result 
+              PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j] <= 'b0;
+              PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j + 1] <= 'b0;
+              PE_result_medium_result_reg[gv_i][STAGE_OUTPUT_BEGIN_INDEX + (gv_j >> 1)] <= 'b0;
+            end
+            else if(~PE_mac_enable) begin // non-calculation time, keep the same
+              PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j] <= PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j];
+              PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j + 1] <= PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j + 1];
+            end
+            else begin // accumulate
+              PE_result_medium_result_reg[gv_i][STAGE_OUTPUT_BEGIN_INDEX + (gv_j >> 1)] <= PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j] + PE_result_medium_result_reg[gv_i][STAGE_INPUT_BEGIN_INDEX + gv_j + 1];
+            end
+          end
+        end
+      end
+
+      // MAC tree logic -- combine the result
+      // assign PE_result_shift_temp = PE_result_medium_result_reg[gv_i][(ACCU_NUM - 1) << 1]; // >>> PE_res_shift_num; // signed extension
+      always @(posedge clk or negedge reset_n) begin
+        if(~reset_n) begin // reset to zero
+          PE_result_out_reg[gv_i] <= 'b0;
+        end
+        else if (PE_clear_acc_reg) begin
+          PE_result_out_reg[gv_i] <= 'b0;
+          for (integer i = 0; i < PE_MEDIUM_RESULT_REG; i = i + 1) begin
+            PE_result_medium_result_reg[gv_i][i] <= 'b0;
+          end
+        end
+        else begin
+          PE_result_out_reg[gv_i] <= PE_result_out_reg[gv_i] + PE_result_medium_result_reg[gv_i][(ACCU_NUM - 1) << 1];  //截取低位
+        end
+      end
+
+      // MAC tree logic -- output
+      assign PE_result_shift_temp = PE_result_out_reg[gv_i] >>> PE_res_shift_num; // signed extension
+      always @(posedge clk or negedge reset_n) begin
+        if(~reset_n) begin
+          PE_result_out[gv_i] <= 'b0;
+        end
+        else if(PE_result_shift_temp > 127) begin
+          PE_result_out[gv_i] <= 127;
+        end
+        else if(PE_result_shift_temp < -128) begin
+          PE_result_out[gv_i] <= -128;
+        end
+        else begin
+          PE_result_out[gv_i] <= PE_result_shift_temp;
         end
       end
     end
   endgenerate
-
-  // MAC tree logic -- combine the result
-  assign PE_result_shift_temp = PE_result_medium_result_reg[MAC_NUM - 1] >>> PE_res_shift_num; // signed extension
-  always @(posedge clk or negedge reset_n) begin
-    if(~reset_n) begin // reset to zero
-      PE_result_out <= 'b0;
-    end
-    else if(PE_result_shift_temp > 127) begin
-      PE_result_out <= 127;
-    end
-    else if(PE_result_shift_temp < -128) begin
-      PE_result_out <= -128;
-    end
-    else begin
-      PE_result_out <= PE_result_out + PE_result_shift_temp[BW_ACT-1:0];  //截取低位
-    end
-  end
-
 endmodule
--- test/tb_MultAddTree.sv
+++ test/tb_MultAddTree.sv
@@ -1,9 +1,10 @@
 `timescale 1ns/1ps
 `define CLK_PERIOD          10 //100MHz
 
-module tb_pe_array; // test bench
+module tb_MultAddTree; // test bench
 
-    parameter MAC_NUM    =   10;  // number of multiply-accumulation units
+    parameter MAC_NUM    =   1;  // number of multiply-accumulation units
+    parameter ACCU_NUM   =   2;  // number of accumulate units
     parameter BW_ACT     =   8;  // bit length of activation
     parameter BW_WET     =   8;  // bit length of weight
     parameter BW_ACCU    =   32;    // bit length of accu result
@@ -15,53 +16,61 @@ module tb_pe_array; // test bench
     parameter OA_H = 100;
     parameter OA_W = 16;
 
+    // parameter IA_H = 8;
+    // parameter IA_W = 8;
+    // parameter Weight_H = 8;
+    // parameter Weight_W = 8;
+    // parameter OA_H = 8;
+    // parameter OA_W = 8;
+
     reg clk;
     reg reset_n;
     reg PE_mac_enable;
     reg PE_clear_acc;
 
-    reg signed [BW_ACT-1:0]    PE_act_in [MAC_NUM-1:0];         // input activation
-    reg signed [BW_WET-1:0]    PE_wet_in;         // input weight
-    reg  [7:0]           PE_res_shift_num;
+    reg xx;
+
+    reg signed  [BW_ACT-1:0]    PE_act_in [MAC_NUM-1:0][ACCU_NUM-1:0];         // input activation
+    reg signed  [BW_WET-1:0]    PE_wet_in [ACCU_NUM-1:0];         // input weight
+    reg         [7:0]           PE_res_shift_num;
     wire signed [BW_ACT-1:0]    PE_result_out [MAC_NUM-1:0];    // output result
 
-    reg signed [BW_ACT-1:0] Input_activation_main_memory [IA_H-1:0][IA_W-1:0]; // main memory (DRAM)
-    reg signed [BW_ACT-1:0] Weight_main_memory [Weight_H-1:0][Weight_W-1:0];
-    reg signed [BW_ACT-1:0] Output_activation_main_memory [OA_H-1:0][OA_W-1:0];
-    reg signed [BW_ACT-1:0] reference_output [OA_H-1:0][OA_W-1:0];
+    reg signed  [BW_ACT-1:0]    Input_activation_main_memory [IA_H-1:0][IA_W-1:0]; // main memory (DRAM)
+    reg signed  [BW_ACT-1:0]    Weight_main_memory [Weight_H-1:0][Weight_W-1:0];
+    reg signed  [BW_ACT-1:0]    Output_activation_main_memory [OA_H-1:0][OA_W-1:0];
+    reg signed  [BW_ACT-1:0]    reference_output [OA_H-1:0][OA_W-1:0];
 
-    //! only for waveform
-    genvar gv_i;
-    for (gv_i = 0; gv_i < MAC_NUM; gv_i = gv_i + 1) begin : line
-        wire signed [BW_ACT-1:0] PE_result_out_tmp;
-        assign PE_result_out_tmp = PE_result_out[gv_i];
-    end
-
-    pe_array #(
-                 .MAC_NUM(MAC_NUM),
-                 .BW_ACT(BW_ACT),
-                 .BW_WET(BW_WET),
-                 .BW_ACCU(BW_ACCU)
-             )u_pe_array(
-                 .clk(clk),
-                 .reset_n(reset_n),
-                 .PE_mac_enable(PE_mac_enable),
-                 .PE_clear_acc(PE_clear_acc),
-                 .PE_act_in(PE_act_in),
-                 .PE_wet_in(PE_wet_in),
-                 .PE_res_shift_num(PE_res_shift_num),
-                 .PE_result_out(PE_result_out)
-             );
+    MultAddTree #(
+                    .MAC_NUM(MAC_NUM),
+                    .ACCU_NUM_LOG2($clog2(ACCU_NUM)),
+                    .BW_ACT(BW_ACT),
+                    .BW_WET(BW_WET),
+                    .BW_ACCU(BW_ACCU)
+                ) u_MultAddTree (
+                    .clk(clk),
+                    .reset_n(reset_n),
+                    .PE_mac_enable(PE_mac_enable),
+                    .PE_clear_acc(PE_clear_acc),
+                    .PE_act_in(PE_act_in),
+                    .PE_wet_in(PE_wet_in),
+                    .PE_res_shift_num(PE_res_shift_num),
+                    .PE_result_out(PE_result_out)
+                );
 
     initial begin
+        xx = 0;
         clk = 0;
         reset_n = 1;
         PE_res_shift_num = 8;
         PE_clear_acc = 0;
-        for(integer n=0;n<MAC_NUM;n=n+1) begin
-            PE_act_in[n] <= '0;
+        for(integer n = 0; n < MAC_NUM; n = n + 1) begin
+            for (integer k = 0; k < ACCU_NUM; k = k + 1) begin
+                PE_act_in[n][k] <= '0;
+            end
         end
-        PE_wet_in <= '0;
+        for (integer k = 0; k < ACCU_NUM; k = k + 1) begin
+            PE_wet_in[k] <= '0;
+        end
         forever begin
             #(`CLK_PERIOD/2) clk = ~clk; //模拟时钟产生
         end
@@ -81,47 +90,76 @@ module tb_pe_array; // test bench
         reset_n = 1;
 
         PE_mac_enable = 1;
-        for(integer m=0;m<OA_W;m=m+1) begin
-            for(integer j=0;j<IA_H/MAC_NUM;j=j+1) begin
-                for(integer i=0;i<IA_W;i=i+1) begin
+        for (integer m = 0; m < OA_W; m = m + 1) begin
+            for (integer j = 0; j < IA_H / MAC_NUM; j = j + 1) begin
+                for (integer i = 0; i < IA_W / ACCU_NUM; i = i + 1) begin
                     @(negedge clk) begin
-                         if(i==0) begin
-                             PE_clear_acc <= 0; // 确保清零工作准时结束
-                         end
-                         for(integer n=0;n<MAC_NUM;n=n+1) begin //相当于课件中的spatial for，在一个周期内完成
-                             PE_act_in[n] <= Input_activation_main_memory[j*MAC_NUM+n][i];
-                         end
-                         PE_wet_in <= Weight_main_memory[i][m];
-                     end
-                 end
-                 @(negedge clk) begin
-                      PE_clear_acc <= 1;
-                  end
-                  @(negedge clk) //结果输出会经过两个周期，因此再打一拍延时
-                   @(negedge clk) begin
-                       for(integer n=0;n<MAC_NUM;n=n+1) begin
-                           Output_activation_main_memory[j*MAC_NUM+n][m] <= PE_result_out[n];
-                       end
-                   end
-               end
-           end
+                        xx <= 0;
+                        if (i == 0) begin
+                            PE_clear_acc <= 0; // clean the zero
+                        end
+                        for (integer n = 0; n < MAC_NUM; n = n + 1) begin // 相当于课件中的spatial for，在一个周期内完成
+                            for (integer accu_i = 0; accu_i < ACCU_NUM; accu_i = accu_i + 1) begin
+                                PE_act_in[n][accu_i] <= Input_activation_main_memory[j*MAC_NUM+n][i*ACCU_NUM+accu_i];
+                            end
+                        end
+                        // PE_wet_in <= Weight_main_memory[i][m];
+                        for (integer accu_i = 0; accu_i < ACCU_NUM; accu_i = accu_i + 1) begin
+                            PE_wet_in[accu_i] <= Weight_main_memory[i*ACCU_NUM+accu_i][m];
+                        end
+                    end
+                end
+                if (IA_W % ACCU_NUM != 0) begin // padding numbers
+                    @(negedge clk) begin
+                        if (IA_W < ACCU_NUM) begin
+                            PE_clear_acc <= 0; // clean the zero
+                        end
+                        for (integer n = 0; n < MAC_NUM; n = n + 1) begin
+                            for (integer accu_i = 0; accu_i < ACCU_NUM; accu_i = accu_i + 1) begin
+                                if (accu_i < IA_W % ACCU_NUM) begin
+                                    PE_act_in[n][accu_i] <= Input_activation_main_memory[j*MAC_NUM+n][(IA_W / ACCU_NUM)*ACCU_NUM+accu_i];
+                                    PE_wet_in[accu_i] <= Weight_main_memory[(IA_W / ACCU_NUM)*ACCU_NUM+accu_i][m];
+                                end
+                                else begin
+                                    PE_act_in[n][accu_i] <= '0;
+                                    PE_wet_in[accu_i] <= '0;
+                                end
+                            end
+                        end
+                    end
+                end
+                    for (integer clock_i = 0; clock_i < $clog2(ACCU_NUM) + 2; clock_i = clock_i + 1) begin
+                        @(negedge clk);
+                    end
+                    @(negedge clk) begin
+                        PE_clear_acc <= 1;
+                    end
+                    @(negedge clk) begin
+                        xx <= 1;
+                        for(integer n = 0; n < MAC_NUM; n = n + 1) begin
+                            Output_activation_main_memory[j*MAC_NUM+n][m] <= PE_result_out[n];
+                        end
+                    end
+                end
+            end
+            @(negedge clk);
            //所有数据都算完后，与reference output进行比对，检验正确与否
            for(integer k=0;k<OA_H;k=k+1) begin
                for(integer p=0;p<OA_W;p=p+1) begin
-                   if(Output_activation_main_memory[k][p]!=reference_output[k][p]) begin
-                       $display("wrong at (%d %d), output %d, reference %d", k, p, Output_activation_main_memory[k][p], reference_output[k][p]);
-                       wrong_num = wrong_num + 1;
-                   end
+                   // if(Output_activation_main_memory[k][p]!=reference_output[k][p]) begin
+                       $display("(%d %d), output %d, reference %d", k, p, Output_activation_main_memory[k][p], reference_output[k][p]);
+                       // wrong_num = wrong_num + 1;
+                   // end
                end
            end
-           $display("wrong num: %d",wrong_num);
+           // $display("wrong num: %d",wrong_num);
         @(negedge clk)
          $finish(0);//仿真结束，自动退出 !!! (important for getting the running time)
     end
 
     initial begin // 加载任务数据（不是相对testbench的路径，而是相对于simv文件的路径）
-        $dumpfile("../vcd/tb_pe_array.vcd");
-        $dumpvars(0, tb_pe_array);
+        $dumpfile("../vcd/tb_MultAddTree.vcd");
+        $dumpvars(0, tb_MultAddTree);
     end
 
 endmodule
